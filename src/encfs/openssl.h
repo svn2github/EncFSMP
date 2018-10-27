a1deebc0d77135a04367b9836b19a1b148b2a476
rhiestan <rhiestan@fe83ab39-2a9f-448e-92a0-1b46b6e07995>
2018-10-27 17:19:10 +0000

    Version 0.99.0, 27.10.2018: - Beta version of 1.0, use with caution! - Upgrade of encfs to 1.9.5 - Update of PFM to 1.0.0.191, boost to 1.68.0, wxWidgets to 3.1.1 and OpenSSL to 1.1.0i on Windows and 1.0.2p on MacOS - Compiling with newest compilers (MinGW GCC 8.1.0 on Windows, Apple-Clang 10.0.0 on MacOS)
    git-svn-id: svn://svn.code.sf.net/p/encfsmp/code/trunk@43 fe83ab39-2a9f-448e-92a0-1b46b6e07995

--- src/encfs/openssl.h
+++ src/encfs/openssl.h
@@ -7,7 +7,7 @@
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.  
+ * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -21,9 +21,11 @@
 #ifndef _openssl_incl_
 #define _openssl_incl_
 
+namespace encfs {
+
 void openssl_init(bool isThreaded);
 void openssl_shutdown(bool isThreaded);
 
+}  // namespace encfs
+
 #endif
-
-
